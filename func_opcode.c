cb50fc5eec5c6902204d3d27febc9fd14ae5c935
AdrianVides56 <adrianvj18@gmail.com>
2020-12-23 11:18:15 -0500

    Hot Fix

--- func_opcode.c
+++ func_opcode.c
@@ -8,58 +8,46 @@
 void f_push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new_node = (stack_t *) malloc(sizeof(stack_t));
-	stack_t *last = NULL;
 
-	last = *stack;
 	if (new_node == NULL)
 	{
 		free(second);
-		_errors(1, line_number, stack);
+		_errors(1, line_number, stack); /* Failed malloc */
 	}
 	if (second == NULL || _parseArg(second) == 0)
 	{
 		free(new_node);
-		_errors(2, line_number, stack);
+		_errors(2, line_number, stack); /* Bad use of push */
 	}
 	if ((isdigit(*second) != 0) || (*second == '-' && isdigit(second[1])))
 		new_node->n = atoi(second);
 
-	new_node->next = NULL;
+	new_node->next = *stack;
 	new_node->prev = NULL;
-	if (*stack == NULL)
-	{
-		new_node->prev = NULL;
-		*stack = new_node;
-	}
-	else
-	{
-	while (last->next != NULL)
-		last = last->next;
-	last->next = new_node;
-	new_node->prev = last;
-	}
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
+
+	(*stack) = new_node;
 }
 
 /**
- * f_pall - prints the stacks, from last to first
+ * f_pall - prints the stacks, from last added to first
  * @head: pointer to list's head
  * @line_number: line number read
  */
-void f_pall(stack_t **head, unsigned int __attribute__((unused)) line_number)
+void f_pall(stack_t **head, unsigned int line_number)
 {
-	stack_t *aux = NULL;
+	stack_t *aux = *head;
 
-	if (*head == NULL)
+	if (aux == NULL)
 		return;
 
-	aux = *head;
-	while (aux->next != NULL)
-		aux = aux->next;
 	while (aux != NULL)
 	{
 		printf("%d\n", aux->n);
-		aux = aux->prev;
+		aux = aux->next;
 	}
+	(void)line_number;
 }
 
 /**
@@ -69,20 +57,10 @@ void f_pall(stack_t **head, unsigned int __attribute__((unused)) line_number)
  */
 void f_pint(stack_t **head, unsigned int line_number)
 {
-	stack_t *aux = NULL;
-
 	if (*head == NULL)
 		_errors(3, line_number, head); /*pint failed*/
 
-	aux = *head;
-	while (aux != NULL)
-	{
-		if (aux->next == NULL)
-		{
-			printf("%d\n", aux->n);
-		}
-		aux = aux->next;
-	}
+	printf("%d\n", (*head)->n);
 }
 /**
  * f_pop - removes the last element added to the stack
@@ -94,7 +72,7 @@ void f_pop(stack_t **head, unsigned int line_number)
 	stack_t *aux = *head;
 
 	if (aux == NULL)
-		_errors(4, line_number, head);
+		_errors(4, line_number, head); /* pop failed */
 
 	else if (aux->prev == NULL && aux->next == NULL)
 	{
@@ -119,7 +97,7 @@ void f_swap(stack_t **stack, unsigned int line_number)
 	int swap_aux;
 
 	if ((*stack) == NULL)
-		_errors(5, line_number, stack);
+		_errors(5, line_number, stack); /* Swap failed */
 	else if ((*stack)->prev == NULL && (*stack)->next == NULL)
 	{
 		free_list(*stack);
